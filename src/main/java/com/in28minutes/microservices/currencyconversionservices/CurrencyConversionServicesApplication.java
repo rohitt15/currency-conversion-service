d59fd050c68c301adcaac0cf8fece47ec2499e3c
rohitt15 <54735391+rohitt15@users.noreply.github.com>
2023-02-07 16:52:07 +0530

    adding the eureka client

--- src/main/java/com/in28minutes/microservices/currencyconversionservices/CurrencyConversionServicesApplication.java
+++ src/main/java/com/in28minutes/microservices/currencyconversionservices/CurrencyConversionServicesApplication.java
@@ -2,8 +2,10 @@ package com.in28minutes.microservices.currencyconversionservices;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class CurrencyConversionServicesApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/in28minutes/microservices/currencyconversionservices/controller/CurrencyConversionController.java
+++ src/main/java/com/in28minutes/microservices/currencyconversionservices/controller/CurrencyConversionController.java
@@ -1,9 +1,13 @@
 package com.in28minutes.microservices.currencyconversionservices.controller;
 
 import com.in28minutes.microservices.currencyconversionservices.entity.CurrencyConversion;
+import com.in28minutes.microservices.currencyconversionservices.proxy.CurrencyExchangeProxy;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
@@ -13,6 +17,9 @@ import java.util.HashMap;
 @RestController
 public class CurrencyConversionController {
 
+    @Autowired
+    private CurrencyExchangeProxy proxy;
+    @ResponseStatus(HttpStatus.ACCEPTED)
     @GetMapping("/currency-conversion/from/{from}/to/{to}/quantity/{quantity}")
     public CurrencyConversion calculateCurrencyConversion(
             @PathVariable String from,
@@ -30,6 +37,20 @@ public class CurrencyConversionController {
                 currencyConversion.getConversionMultiple(),
                 quantity.multiply(currencyConversion.getConversionMultiple()),
                 currencyConversion.getEnvironment());
+
+    }
+
+    @GetMapping("/currency-conversion-feign/from/{from}/to/{to}/quantity/{quantity}")
+    public CurrencyConversion calculateCurrencyConversionFeign(
+            @PathVariable String from,
+            @PathVariable String to,
+            @PathVariable BigDecimal quantity){
+
+        CurrencyConversion currencyConversion = proxy.retrieveExchangeValue(from, to);
+        return new CurrencyConversion(currencyConversion.getId(), from,to,quantity,
+                currencyConversion.getConversionMultiple(),
+                quantity.multiply(currencyConversion.getConversionMultiple()),
+                currencyConversion.getEnvironment()+" "+"feign");
 
     }
 }
