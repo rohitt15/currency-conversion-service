e009e742ed90d06b285978c5f00a8ffe14dfab7f
rohitt15 <54735391+rohitt15@users.noreply.github.com>
2023-02-07 10:56:49 +0530

    initial commit

---
+++ src/main/java/com/in28minutes/microservices/currencyconversionservices/CurrencyConversionServicesApplication.java
@@ -0,0 +1,13 @@
+package com.in28minutes.microservices.currencyconversionservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrencyConversionServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurrencyConversionServicesApplication.class, args);
+	}
+
+}
